145766fc3894454e51baa6641a1c34733bc53121
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-02 20:13:36 +0600

    Fetched todo to task array

---
+++ Vue Test/vue-test/src/App1.vue
@@ -0,0 +1,72 @@
+<script setup>
+
+import { onMounted, ref } from 'vue';
+
+    const name = ref ('Tanjim Taosif');
+    const status = ref ('active');
+    const tasks = ref (['Task One', 'Task Two', 'Task Three']);
+    const link = ref ('https://www.google.com/');
+    const newTask = ref('');
+
+    const toggleStatus = () => {
+      if (status.value === 'active'){
+        status.value = 'pending'
+      } else if (status.value === 'pending'){
+        status.value = 'inactive';
+      } else {
+        status.value = 'active'
+      }
+    };
+
+    const addTask = () => {
+      if (newTask.value.trim() !== '') {
+        tasks.value.push(newTask.value);
+        newTask.value = '';
+      }
+    };
+
+    const deleteTask = (index) => {
+      tasks.value.splice(index, 1);
+    }
+
+    onMounted(async () => {
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await response.json();
+        tasks.value = data.map((task) => task.title);
+      } catch (error){
+        console.log('Error fetching tasks');
+        
+      };
+    })
+</script>
+
+<template>
+<h1>{{name}}</h1>
+<p v-if="status === 'active'" >User is active</p>
+<p v-else-if="status === 'pending'" >User is pending</p>
+<p v-else="status">User is inactive</p>
+
+<form @submit.prevent="addTask">
+  <label for="newTask">Add Task</label>
+  <input type="text" id="newTask" name="newTask" v-model="newTask">
+  <button type="submit">Submit</button>
+</form>
+
+<h2>Tasks:</h2>
+<ul>
+  <li v-for="(task, index) in tasks" :key="index">
+  <span>{{ task }}</span>
+  <button @click="deleteTask(index)">x</button>
+</li>
+</ul>
+
+<a v-bind:href="link">CLick for Google</a> <br>
+
+<button @click="toggleStatus">Change Status</button>
+</template>
+
+<style>
+
+</style>
+
