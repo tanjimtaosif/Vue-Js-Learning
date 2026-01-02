f9da5a73733aca442e89470982ecb2a87ae6147f
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-02 22:22:12 +0600

    Updated

--- Vue Test/vue-test/src/components/JobListing.vue
+++ Vue Test/vue-test/src/components/JobListing.vue
@@ -44,7 +44,7 @@ const truncatedDescription = computed(() => {
                 </div>
                 <a
                   :href="'/job/' + job.id"
-                  class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                  class="h-9 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                   Read More
                 </a>
