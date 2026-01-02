fcb773a5a94bac2797bd145121095fac8d89cae2
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-02 19:51:41 +0600

    Fetched Json data to the task array

--- Vue Test/vue-test/src/App.vue
+++ Vue Test/vue-test/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
     const name = ref ('Tanjim Taosif');
     const status = ref ('active');
@@ -28,6 +28,17 @@ import { ref } from 'vue';
     const deleteTask = (index) => {
       tasks.value.splice(index, 1);
     }
+
+    onMounted(async () => {
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await response.json();
+        tasks.value = data.map((task) => task.title);
+      } catch (error){
+        console.log('Error fetching tasks');
+        
+      };
+    })
 </script>
 
 <template>
