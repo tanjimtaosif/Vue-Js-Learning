5597c23267e08ded71240e067012ba84bf72d33a
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-03 14:11:57 +0600

    Redndered item from items array

--- Vue.js 3 Fundamentals/vue-fundamentals/src/components/ShoppingListApp.vue
+++ Vue.js 3 Fundamentals/vue-fundamentals/src/components/ShoppingListApp.vue
@@ -2,7 +2,15 @@
 import {ref} from 'vue'
 
 const header = ref('Shopping List App')
+const items = ref ([
+    {id:1, label:"10 Car"}, 
+    {id:2, label:"20 Bikes"}, 
+    {id:3, label:"3 EVs"}
+])
 </script>
 <template>
 <h1>{{ header }}</h1>
+<ul>
+    <li v-for="item in items" key="item.id">{{ item.label }}</li>
+</ul>
 </template>
