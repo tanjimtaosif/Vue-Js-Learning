cdabeb45fd6848d6a430ed596946929e1de09848
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-03 19:59:31 +0600

    Tried the vue reactiviy by making a simple counter app in vue

--- Vue.js 3 Fundamentals/vue-fundamentals/src/App.vue
+++ Vue.js 3 Fundamentals/vue-fundamentals/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
+import Reactivity from './components/reactivity.vue';
 import ShoppingListApp from './components/ShoppingListApp.vue';
 </script>
 
 <template>
+  <Reactivity />
   <!-- <HelloWorld msg="Vue Js Fundamentals" /> -->
   <ShoppingListApp/>
    
