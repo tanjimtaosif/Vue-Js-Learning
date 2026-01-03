a2fcec68c4f2946f536b12481393080fc51cfb47
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-03 13:35:46 +0600

    Updated

--- Vue.js 3 Fundamentals/vue-fundamentals/src/components/HelloWorld.vue
+++ Vue.js 3 Fundamentals/vue-fundamentals/src/components/HelloWorld.vue
@@ -2,9 +2,13 @@
 import { ref } from 'vue'
 
 const msg = ref('Hello World!')
+// const txt = ref('Welcome')
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <!-- We can do one expression at a time -->
+  <!--Use txt.toLocalUppercase to uppercase the txt -->
+  <h1>{{ txt || 'Hi There!!!'}}</h1>
   <input v-model="msg" />
 </template>
