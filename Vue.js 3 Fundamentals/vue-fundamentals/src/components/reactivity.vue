dec3fd3c0716e12027496c9012395f02316df269
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-03 20:00:38 +0600

    Tried to make a counter app using vue reactivity

---
+++ Vue.js 3 Fundamentals/vue-fundamentals/src/components/reactivity.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import { ref, reactive } from 'vue';
+    // reactive don't work with premitive 
+    // Also state can't be string or number
+    // Also it can't be boolean
+    const state = reactive({count: 0})
+
+    const increament = () => {
+         state.count++
+        
+    }
+    const msg = ref("Counter App in Vue")
+    console.log(msg.value);
+    
+</script>
+
+<template>
+    <h1>
+    {{ msg }}
+    {{  state.count}}
+    </h1>
+    <button @click="increament">
+        Increament Count
+    </button>
+</template>
