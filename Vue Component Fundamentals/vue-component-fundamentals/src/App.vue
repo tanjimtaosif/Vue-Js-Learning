ac6297fa142bbfa7ce16f73c7e3f74f5a9a263fc
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-05 21:40:03 +0600

    Made the plans dynamic

--- Vue Component Fundamentals/vue-component-fundamentals/src/App.vue
+++ Vue Component Fundamentals/vue-component-fundamentals/src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
-import CounterButton from './components/CounterButton.vue';
+import CoffePlans from './components/CoffePlans.vue';
+// import CounterButton from './components/CounterButton.vue';
+import { ref } from 'vue';
+const plans = ref (["The Hacker","The Single","The Curious","The Addict" ]);
 </script>
 
 <template>
-  <CounterButton />
+  <!-- <CounterButton /> -->
+  
+
+  <div class="content">
+  <h1 class="title">Coffee Plans</h1>
+
+  <h2 class="subtitle">We travel the world to source the very best single origin coffee for you</h2>
+
+  <div class="plans">
+    <CoffePlans v-for =" plan in plans" :key="plan" :name="plan"></CoffePlans>
+
+  </div>
+</div>
 </template>
 
 <style scoped>
---
+++ Vue Component Fundamentals/vue-component-fundamentals/src/components/CoffePlans.vue
@@ -0,0 +1,15 @@
+<script setup>
+    defineProps(["name"])
+</script>
+
+<template>
+    <div class="plan">
+      <div class="description">
+        <!-- Using props -->
+        <span class="title"> {{ name }} </span> 
+      </div>
+    </div>
+</template>
+<style>
+
+</style>
