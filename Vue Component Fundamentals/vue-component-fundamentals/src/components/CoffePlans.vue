cbd1befdf536eeae9a3d3e94700bba39d3856937
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 13:27:43 +0600

    Now the program shows selected value

--- Vue Component Fundamentals/vue-component-fundamentals/src/components/CoffePlans.vue
+++ Vue Component Fundamentals/vue-component-fundamentals/src/components/CoffePlans.vue
@@ -1,14 +1,14 @@
 <script setup>
-    defineProps({
-        name : {
-            type : String,
-            default : 'Default Plan'
-        }
+  import { ref } from 'vue';
+   const props = defineProps({
+        name : {type : String, default : 'Default Plan'},
+        selected : {type: Boolean, default: false}
     })
+
 </script>
 
 <template>
-    <div class="plan">
+    <div class="plan" :class="{'active-plan' : selected}">
       <div class="description">
         <!-- Using props -->
         <span class="title"> {{ name }} </span> 
