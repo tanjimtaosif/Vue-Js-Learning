585ae9626548b2354215589ad82403e219ae5380
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 13:28:12 +0600

    Shows active only selected value

--- Vue Component Fundamentals/vue-component-fundamentals/src/components/PlanPicker.vue
+++ Vue Component Fundamentals/vue-component-fundamentals/src/components/PlanPicker.vue
@@ -1,19 +1,24 @@
 <script setup>
 import { ref } from 'vue';
-// import CoffePlans from './CoffePlans.vue';
+import CoffePlans from './CoffePlans.vue';
 const plans = ref (["The Hacker","The Single","The Curious","The Addict" ]);
+
+const selectedCoffeePlan = ref()
+
+function handleSelectedCoffeePlan(name){
+  selectedCoffeePlan.value= name
+}
 </script>
 
 <template>
-
-<div class="content">
-  <h1 class="title">Coffee Plans</h1>
-
-  <h2 class="subtitle">We travel the world to source the very best single origin coffee for you</h2>
-
   <div class="plans">
-    <CoffePlans v-for =" plan in plans" :key="plan" :name="plan"></CoffePlans>
-
+    {{ selectedCoffeePlan }}
+    <CoffePlans 
+    v-for =" plan in plans" 
+    :key="plan" 
+    :name="plan" 
+    :selected="plan === selectedCoffeePlan"
+    @click="$event => handleSelectedCoffeePlan(plan)"></CoffePlans>
   </div>
-  </div>
+  
 </template>
