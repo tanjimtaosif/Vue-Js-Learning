31de3e7886cc557b4806272e4dacc9606a9a168f
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-13 17:02:45 +0600

    Button componenet created

---
+++ restaurant-demo/src/components/ui/BaseButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+}>()
+</script>
+<template>
+  <button class="base-button">
+    {{ label }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/styles/abstracts/variables' as vars;
+
+.base-button {
+  padding: 0.75rem 2rem;
+  background-color: vars.$color-gold;
+  color: vars.$color-black;
+  border: none;
+  border-radius: vars.$radius-md;
+  font-weight: 600;
+  cursor: pointer;
+}
+</style>
