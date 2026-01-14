2561a2d3605281f49b87ff4c94047c8ee0b97fb2
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-14 16:02:15 +0600

    Added navbar to our app.vue

--- restaurant-demo/src/App.vue
+++ restaurant-demo/src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-  import BaseContainer from './components/ui/BaseContainer.vue';
+import AppNavbar from '@/components/layout/AppNavbar.vue'
+import BaseContainer from './components/ui/BaseContainer.vue'
+import AppFooter from './components/layout/AppFooter.vue'
 </script>
 
 <template>
+  <AppNavbar />
+
   <BaseContainer>
     <router-view />
   </BaseContainer>
+  <AppFooter />
 </template>
-
 <style scoped></style>
