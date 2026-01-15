f437355352b2f60ab05fe4c2cccdd2ef43f8c9c8
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-15 17:19:28 +0600

    Footer added

--- i-restaurant/src/App.vue
+++ i-restaurant/src/App.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppFooter from './components/layout/AppFooter.vue'
+import AppHeader from './components/layout/AppHeader.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <AppHeader />
+  <AppFooter />
 </template>
 
 <style scoped></style>
---
+++ i-restaurant/src/components/layout/AppFooter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import BaseContainer from '../base/BaseContainer.vue';
+
+  const year = new Date().getFullYear()
+</script>
+<template>
+  <footer class="app-footer">
+    <BaseContainer>
+       <div class="app-footer__content">
+        <p class="app-footer__copy">
+          © {{ year }} Irestaurant. All rights reserved.
+        </p>
+       </div>
+    </BaseContainer>
+  </footer>
+</template>
