44971cd5983a6ac5f94680036af69e29d5b8d0bc
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-15 21:12:25 +0600

    Hero section and features section added

--- i-restaurant/src/App.vue
+++ i-restaurant/src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import AppFooter from './components/layout/AppFooter.vue'
 import AppHeader from './components/layout/AppHeader.vue'
+import FeaturesSection from './components/sections/FeaturesSection.vue'
+import HeroSection from './components/sections/HeroSection.vue'
 </script>
 
 <template>
   <AppHeader />
+  <HeroSection />
+  <FeaturesSection />
   <AppFooter />
 </template>
 
