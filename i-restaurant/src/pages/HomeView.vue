29de6b83e354cb99d5656af261a135c31c5f04e8
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-16 16:23:15 +0600

    Add `FeaturesSection` and `FeatureDetailsSection` components, and integrate `FeaturesSection` into `HomeView`.

--- i-restaurant/src/pages/HomeView.vue
+++ i-restaurant/src/pages/HomeView.vue
@@ -1,4 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HeroSection from '@/components/sections/HeroSection.vue'
+// import FeatureDetailsSection from '@/components/sections/FeatureDetailsSection.vue'
+import FeaturesSection from '@/components/sections/FeaturesSection.vue';
+</script>
+
 <template>
-  <section></section>
+  <HeroSection />
+  <FeaturesSection />
+  <!-- <FeatureDetailsSection /> -->
 </template>
