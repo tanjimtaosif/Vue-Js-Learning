c6d54eecaf59342e612f2c32321cbf5a56ad2ee0
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-17 00:07:21 +0600

    add ComparisonSection to HomeView for enhanced feature comparison

--- i-restaurant/src/pages/HomeView.vue
+++ i-restaurant/src/pages/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import HeroSection from '@/components/sections/HeroSection.vue'
 // import FeatureDetailsSection from '@/components/sections/FeatureDetailsSection.vue'
-import FeaturesSection from '@/components/sections/FeaturesSection.vue';
+import FeaturesSection from '@/components/sections/FeaturesSection.vue'
+import ComparisonSection from '@/components/sections/ComparisonSection.vue'
+import GallerySection from '@/components/sections/GallerySection.vue'
+import TestimonialsSection from '@/components/sections/TestimonialsSection.vue';
 </script>
 
 <template>
   <HeroSection />
   <FeaturesSection />
   <!-- <FeatureDetailsSection /> -->
+  <ComparisonSection />
+  <GallerySection />
+  <TestimonialsSection />
 </template>
