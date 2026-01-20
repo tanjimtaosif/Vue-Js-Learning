21702f84f921260c91146481b35d34b5a321c052
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-20 14:38:44 +0600

    Fix import paths in HomeView to use correct directory structure for section components

--- i-restaurant/src/pages/HomeView.vue
+++ i-restaurant/src/pages/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import HeroSection from '@/components/sections/HeroSection.vue'
-import FeaturesSection from '@/components/sections/FeaturesSection.vue'
-import ComparisonSection from '@/components/sections/ComparisonSection.vue'
-import GallerySection from '@/components/sections/GallerySection.vue'
-import TestimonialsSection from '@/components/sections/TestimonialsSection.vue'
-import CtaSection from '@/components/sections/CtaSection.vue'
+import HeroSection from '@/components/sections/home/HeroSection.vue'
+import FeaturesSection from '@/components/sections/home/FeaturesSection.vue'
+import ComparisonSection from '@/components/sections/home/ComparisonSection.vue'
+import GallerySection from '@/components/sections/home/GallerySection.vue'
+import TestimonialsSection from '@/components/sections/home/TestimonialsSection.vue'
+import CtaSection from '@/components/sections/home/CtaSection.vue'
 </script>
 
 <template>
